c1cae17be723b6eab35b284c94afcad8c239e55e
VAISHNAVI-R-V <vaishucv14@gmail.com>
2021-11-04 10:59:44 +0530

    REFACTORED: Code with Java Documentations.

--- src/main/java/com/invoice/InvoiceGenerator.java
+++ src/main/java/com/invoice/InvoiceGenerator.java
@@ -1,16 +1,28 @@
 package com.invoice;
 
+/**
+ * Purpose : To implement Cab Service.
+ *
+ * @author VAISHNAVI R VISHWAKARMA
+ * @since 2021-11-04
+ */
 public class InvoiceGenerator {
 
     private static final int COST_PER_TIME = 1;
     private static final double MINIMUM_COST_PER_KILOMETER = 10.0;
     private static final double MINIMUM_FARE = 5;
 
+    /**
+     * Purpose : To calculate the minimum fare.
+     *
+     * @param distance : takes the distance.
+     * @param time     : takes the time.
+     * @return total fare or minimum fare.
+     */
     public double calculateFare(double distance, int time) {
 
         double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
         return Math.max(totalFare, MINIMUM_FARE);
     }
-
-
+    
 }
